60861898ff1ba876472ab70360d4cf4739278892
Laura <lmmcampbell@gmail.com>
2020-11-25 15:52:20 -0600

    updatestomodels

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Artist, Album, Song} = require('../server/db/models')
+const {Artist, Album, Song} = require('../server/db/models')
 
 const songArray = [
   {
@@ -194,11 +194,6 @@ async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
-
   const artists = await Promise.all(
     artistArray.map(artist => {
       return Artist.create(artist)
@@ -217,7 +212,6 @@ async function seed() {
     })
   )
 
-  console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
 
--- server/api/music.js
+++ server/api/music.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 const {Parser} = require('node-sql-parser')
 const parser = new Parser()
+const db = require('../db')
 
 router.get('/', async (req, res, next) => {
   try {
@@ -10,6 +11,44 @@ router.get('/', async (req, res, next) => {
     ) // mysql sql grammer parsed by default
     console.log(ast)
     res.send(ast)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/models', async (req, res, next) => {
+  try {
+    const [results, metadata] = await db.query(
+      "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema='public'"
+    )
+    // let tableNames = results.map(async (element) =>
+    //   {
+    //     let tableName = 'songs';
+    //     console.log("ZEBRA", tableName)
+    //     const [columnArray, columnMetadata] = await db.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'songs'")
+    //     const mapped = columnArray.map((element2) => element2.column_name)
+    //     console.log("HELLO", columnArray)
+    //   });
+    let prettierArray = results.reduce(
+      (accum, element) => {
+        for (let i = 0; i < accum.length; i++) {
+          let current = accum[i]
+          let tableName = element.table_name
+          let columnName = element.column_name
+          if (current[tableName]) {
+            console.log('PANDA', current, tableName, columnName)
+            //current[tableName].push(columnName)
+          } else {
+            let newObj = {}
+            newObj[tableName] = columnName
+            accum.push(newObj)
+          }
+        }
+        return accum
+      },
+      ['test']
+    )
+    res.send(prettierArray)
   } catch (err) {
     next(err)
   }
