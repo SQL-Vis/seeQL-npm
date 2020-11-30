6dbbbf93c522442e9ced5b3b9240d8ed36fb9134
Vivian <vivianxu97@protonmail.com>
2020-11-30 11:36:11 -0600

    added lodash

--- server/api/music.js
+++ server/api/music.js
@@ -3,6 +3,7 @@ module.exports = router
 const {Parser} = require('node-sql-parser')
 const parser = new Parser()
 const db = require('../db')
+var _ = require('lodash')
 
 router.get('/', async (req, res, next) => {
   try {
@@ -40,14 +41,22 @@ router.get('/models', async (req, res, next) => {
       },
       ['test']
     )
-    //redo with lodash
-    let songsArray = prettierArray.slice(1)[0].songs.sort()
-    songsArray.unshift(songsArray.splice(songsArray.indexOf('id'), 1))
-    let albumsArray = prettierArray.slice(1)[1].albums.sort()
-    albumsArray.unshift(albumsArray.splice(albumsArray.indexOf('id'), 1))
-    let artistsArray = prettierArray.slice(1)[2].artists.sort()
-    artistsArray.unshift(artistsArray.splice(artistsArray.indexOf('id'), 1))
-    res.send(prettierArray.slice(1))
+    //redid with lodash
+    let result = prettierArray.slice(1)
+    _.forEach(result, function(value) {
+      _.forEach(value, function(value, key) {
+        value.sort().unshift(value.splice(value.indexOf('id'), 1)[0])
+      })
+    })
+    res.send(result)
+    //original script
+    // let songsArray = prettierArray.slice(1)[0].songs.sort()
+    // songsArray.unshift(songsArray.splice(songsArray.indexOf('id'), 1))
+    // let albumsArray = prettierArray.slice(1)[1].albums.sort()
+    // albumsArray.unshift(albumsArray.splice(albumsArray.indexOf('id'), 1))
+    // let artistsArray = prettierArray.slice(1)[2].artists.sort()
+    // artistsArray.unshift(artistsArray.splice(artistsArray.indexOf('id'), 1))
+    // res.send(prettierArray.slice(1))
   } catch (err) {
     next(err)
   }
