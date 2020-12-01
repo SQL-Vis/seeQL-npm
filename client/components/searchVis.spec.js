fc1b98caafe0169b786b17d38109e8017f5cc070
Vivian <vivianxu97@protonmail.com>
2020-12-01 13:35:03 -0600

    finished initial test

---
+++ client/components/searchVis.spec.js
@@ -0,0 +1,77 @@
+import {expect} from 'chai'
+import sinon from 'sinon'
+import {mount, shallow, configure} from 'enzyme'
+import {SearchVis} from './searchVis'
+const {JSDOM} = require('jsdom')
+import mockAxios from './mock-axios'
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const {window} = jsdom
+global.window = window
+global.document = window.document
+global.navigator = {
+  userAgent: 'node.js'
+}
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+configure({adapter: new Adapter()})
+
+describe('searchVis component', () => {
+  const getSearchVisSpy = sinon.spy()
+  const queryArr = [
+    {songs: ['artistId', 'length', 'title']},
+    {albums: ['genre', 'title', 'label']},
+    {
+      artists: ['id', 'name', 'age']
+    }
+  ]
+  beforeEach(() => {
+    mockAxios.onGet('/api/music/models').replyOnce(200, queryArr)
+  })
+
+  describe('searchVis component', () => {
+    afterEach(() => {
+      getSearchVisSpy.resetHistory()
+    })
+
+    it('renders the models as tables', () => {
+      const wrapper = shallow(
+        <SearchVis getModels={getSearchVisSpy} tables={queryArr} />
+      )
+      expect(wrapper.find('table')).to.have.length(3)
+      expect(wrapper.text()).to.include('genre')
+      expect(wrapper.text()).to.include('title')
+    })
+
+    it('highlights column name cell when a simple select query is inputted', () => {
+      const sampleQueryVis = {select: ['albumslabel']}
+      const wrapper = shallow(
+        <SearchVis
+          queryVis={sampleQueryVis}
+          getModels={getSearchVisSpy}
+          tables={queryArr}
+        />
+      )
+      expect(wrapper.find('.highlighted')).to.have.length(1)
+      expect(wrapper.find('.highlighted').text()).to.equal('label')
+      expect(wrapper.find('.highlighted').text()).to.not.equal('age')
+    })
+  })
+
+  it('highlights column name cells when a join query is inputted', () => {
+    const sampleQueryVis = {
+      join: {left: 'songsartistId', right: 'artistsId', type: 'RIGHT JOIN'},
+      select: ['albumslabel', 'artistsage']
+    }
+    const wrapper = shallow(
+      <SearchVis
+        queryVis={sampleQueryVis}
+        getModels={getSearchVisSpy}
+        tables={queryArr}
+      />
+    )
+    expect(wrapper.find('.highlighted')).to.have.length(2)
+    // expect(wrapper.find('.highlighted').text()).to.equal('age')
+    // expect(wrapper.find('.highlighted').text()).to.equal('title')
+    expect(wrapper.find('.highlighted').text()).to.not.equal('genre')
+  })
+})
