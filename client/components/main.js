9a94f1e84f95500d9669cd8487cc1219e17d75a7
Vivian <vivianxu97@protonmail.com>
2020-12-08 11:57:53 -0600

    added footer styling

--- client/components/main.js
+++ client/components/main.js
@@ -15,33 +15,35 @@ export class Main extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="topDiv">
-          <div className="topLeftDiv">
-            <QueryInput />
-            <QueryHistory />
-            <CurrentSearch />
-          </div>
-          <div className="topRightDiv">
-            <Key />
-          </div>
-        </div>
-        <div className="sectionBox">
-          <div className="sectionTitle">
-            Database Schema & Search Visualization
-          </div>
-          <div className="queryVisBox">
-            <SearchVis />
-          </div>
-        </div>
-        {this.props.result.columns && (
-          <div className="sectionBox">
-            <div className="sectionTitle">Query Result</div>
-            <div className="resultBox">
-              <Result />
+      <div>
+        <div className="container">
+          <div className="topDiv">
+            <div className="topLeftDiv">
+              <QueryInput />
+              <QueryHistory />
+              <CurrentSearch />
+            </div>
+            <div className="topRightDiv">
+              <Key />
             </div>
           </div>
-        )}
+          <div className="sectionBox">
+            <div className="sectionTitle">
+              Database Schema & Search Visualization
+            </div>
+            <div className="queryVisBox">
+              <SearchVis />
+            </div>
+          </div>
+          {this.props.result.columns && (
+            <div className="sectionBox">
+              <div className="sectionTitle">Query Result</div>
+              <div className="resultBox">
+                <Result />
+              </div>
+            </div>
+          )}
+        </div>
         <Footer />
       </div>
     )
